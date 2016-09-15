2cd91e22801116c4fd63bea952e8693645ab3e31
Fernando Abarca <fernando.abarca@gmail.com>
2016-09-15 10:11:04 +0200

    fix createTempDir

--- R/createTempDir.R
+++ R/createTempDir.R
@@ -7,12 +7,20 @@
 #' @param length_random_suffix Optional. Number of random chars that will be used as suffix. By default 10.
 #' @export
 #' @examples
-#' temporary_folder = createTempDir(temp_folder,"raster2psql")
-#' on.exit(unlink(temporary_folder,recursive = TRUE),add=TRUE)
+#'
+#' some.function <- function(..., temp_folder = '/default/path'){
+#'   temporary_folder = createTempDir(temp_folder,"prefix_str")
+#'   print(temporary_folder)
+#'   # this will remove the temporary folder at the end of function execution
+#'   on.exit(unlink(temporary_folder,recursive = TRUE),add=TRUE)
+#'
+#'   # Some code
+#'
+#' }
 
 createTempDir<-function(parent_dir, temp_dir_prefix = "", length_random_suffix = 10){
   if(!file.exists(parent_dir)){
-    stop(paste0("ERROR parent_dir does not exist: ", parent_dir))
+    stop("ERROR parent_dir does not exist: '", parent_dir, "'")
   }
 
   dir_created = FALSE
@@ -20,14 +28,15 @@ createTempDir<-function(parent_dir, temp_dir_prefix = "", length_random_suffix =
   while(!dir_created){
     random_chars = sample(c(letters, toupper(letters), 0:9), length_random_suffix, replace=TRUE)
     randomStr = paste(random_chars, collapse = "")
-    temporary_dir = paste(parent_dir, "/", temp_dir_prefix, "_", randomStr, "/", sep = "")
+    temporary_dir = paste0(parent_dir, "/", temp_dir_prefix, "_", randomStr)
 
     if(!file.exists(temporary_dir)){
       dir.create(temporary_dir)
       if(!file.exists(temporary_dir)){
-        stop("ERROR temporary directory could not be created:", temporary_dir)
+        stop("ERROR temporary directory could not be created: '", temporary_dir, "'")
       }
       dir_created = TRUE
+      temporary_dir = paste0(temporary_dir, "/")
     }
 
   }
