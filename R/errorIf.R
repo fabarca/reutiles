c8f8bc3abcec288b4def8065d97d7c180dd2add1
Fernando Abarca <fernando.abarca@gmail.com>
2017-02-07 11:46:56 +0100

    fix message errorIf

--- R/errorIf.R
+++ R/errorIf.R
@@ -10,12 +10,12 @@ errorIf<-function(x){
   if(x){
     mc = match.call()
     x_name = deparse(mc[[2]])
-    error_str = paste0('"', x_name, '" is TRUE ')
+    error_str = paste0('Condition [ ', x_name, ' ] is TRUE ')
     parent_call = sys.call(-1)
 
     if(!is.null(parent_call)){
-      parent_function = deparse(parent_call)
-      error_str = paste0('in function "', parent_function, '". ',error_str)
+      parent_function = paste0(deparse(parent_call), collapse='')
+      error_str = paste0('in function [ ', parent_function, ' ] -- ',error_str)
     }
     stop(error_str, call. = FALSE)
   }
